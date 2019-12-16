9ab02930b6dc753507f953b25f647e65be90f625
Wolfram Diestel <diestel@steloj.de>
2019-12-16 22:11:35 +0100

    estu pli leĝera pri la IP/reto por PASV

--- bin/setup_vsftpd.sh
+++ bin/setup_vsftpd.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
-sed -i "s/[# ]*pasv_address\s*=.*/pasv_address=${PASV_ADDRESS}/" /etc/vsftpd.conf
+# https://bobcares.com/blog/227-entering-passive-mode-vsftpd/
+
+## rezignu pri limigo de interfaco...
+# PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
+# sed -i "s/[# ]*pasv_address\s*=.*/pasv_address=${PASV_ADDRESS}/" /etc/vsftpd.conf
 
 FTP_PASSWD=$(cat /run/secrets/voko-sesio.ftp_password)
 echo "sesio:${FTP_PASSWD}"|chpasswd
